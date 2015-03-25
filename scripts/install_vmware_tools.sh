30a5032713b06001436a9872fabaea3b82f3e2de
Tyler Tidman <tyler.tidman@draak.ca>
2015-03-25 09:44:22 -0400

    Attempt to provide stub vmware guest tools installer script and use long options

--- scripts/install_vmware_tools.sh
+++ scripts/install_vmware_tools.sh
@@ -2,19 +2,19 @@
 
 # Attempt to install the proprietary VM guest additions/tools thing.
 
-iso_name="${HOME}/VBoxGuestAdditions_$(cat ${HOME}/.vbox_version).iso"
-mount_location="/media/vbox_tmp"
+iso_name="${HOME}/linux.iso"
+mount_location="/media/vmwf_tmp"
 
 # Don't try to install anything if stuff isn't available.
 if [ ! -r ${iso_name} ]; then exit; fi
 
 # Pull in an entire development environment just to make the installer happy.
-apt-get --yes install build-essential dkms
+apt-get --yes install build-essential linux-headers-$(uname -r)
 
 mkdir --parents ${mount_location}
 mount --options loop ${iso_name} ${mount_location}
 
-echo "yes" | bash ${mount_location}/VBoxLinuxAdditions.run
+#run install thingy and interact with the extremely chatty installer
 
 umount ${mount_location}
 rm --recursive --force ${mount_location}
