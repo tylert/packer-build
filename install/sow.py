4bd8e83897df27bac687e3f4831fb397736cda66
Tyler Tidman <tyler.tidman@draak.ca>
2015-04-04 23:52:52 -0400

    Add in a quick and dirty python http server to work around the problem of trying to make debian installer behave sensibly

---
+++ install/sow.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# https://docs.python.org/3.4/library/http.server.html
+# https://docs.python.org/2.7/library/simplehttpserver.html
+
+import sys
+if sys.version[:1] == '3':
+    import http.server
+    import socketserver
+else:
+    import SimpleHTTPServer
+    import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(('', PORT), Handler)
+
+print('serving at port {0}'.format(PORT))
+httpd.serve_forever()
