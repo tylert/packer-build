da6b2e8d1ad8729f000948cc3d95435584988e58
Tyler Tidman <tyler.tidman@draak.ca>
2016-01-22 11:37:46 -0500

    Make the template checker spit out what it is doing

--- check_templates.sh
+++ check_templates.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 for template in $(find {debian,ubuntu} -name '*.json'); do
+    echo "Verifying ${template}"
     packer fix ${template} > ${template}.new
     mv ${template}.new ${template}
 done
