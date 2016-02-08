d472004deae94e409aff80c5c7150abaf3f1467c
Tyler Tidman <tyler.tidman@draak.ca>
2016-02-08 16:18:36 -0500

    Get rid of redundant template check script

--- check_templates.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for template in $(find {debian,ubuntu} -name '*.json'); do
-    echo "Verifying ${template}"
-    packer fix ${template} > ${template}.new
-    mv ${template}.new ${template}
-done
