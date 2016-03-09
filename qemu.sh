4e1d503c1071762fa761a2fc48a3470d600fe816
Tyler Tidman <tyler.tidman@draak.ca>
2016-03-09 09:01:38 -0500

    Put in some better handling in case of spaces in dir name

--- qemu.sh
+++ qemu.sh
@@ -2,9 +2,9 @@
 
 packer_cache_dir="${HOME}/packer_cache"
 
-if [ ! -d ${packer_cache_dir} ]; then
-    mkdir -p ${packer_cache_dir}
+if [ ! -d "${packer_cache_dir}" ]; then
+    mkdir -p "${packer_cache_dir}"
 fi
 
-PACKER_CACHE_DIR=${packer_cache_dir} CHECKPOINT_DISABLE=1 \
+PACKER_CACHE_DIR="${packer_cache_dir}" CHECKPOINT_DISABLE=1 \
 packer build -only=qemu ${@}
