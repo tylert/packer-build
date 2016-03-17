fbae5e91c2d61a6104187755badb142085d496c8
Tyler Tidman <tyler.tidman@draak.ca>
2016-03-17 11:41:42 -0400

    Rename AWS user vars to group them better

--- generate_templates.sh
+++ generate_templates.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 for template in $(find {debian,ubuntu} -name '*.yaml'); do
+    if [ ! -f "${template}" ]; then continue; fi
     echo "Generating ${template/yaml/json}"
-    ./yaml2json.rb < ${template} > ${template/yaml/json}
-    packer fix ${template/yaml/json} > ${template/yaml/json}.new
-    mv ${template/yaml/json}.new ${template/yaml/json}
+    ./yaml2json.rb < "${template}" > "${template/yaml/json}"
+    packer fix "${template/yaml/json}" > "${template/yaml/json}.new"
+    mv "${template/yaml/json}.new" "${template/yaml/json}"
 done
