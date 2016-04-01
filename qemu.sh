65eb27b304582bdd61fe0f1e28d68a797b31e575
Tyler Tidman <tyler.tidman@draak.ca>
2016-04-01 13:22:35 -0400

    Keep track of a quick way to turn on logging

--- qemu.sh
+++ qemu.sh
@@ -6,5 +6,6 @@ if [ ! -d "${packer_cache_dir}" ]; then
     mkdir -p "${packer_cache_dir}"
 fi
 
+#PACKER_LOG=1 PACKER_LOG_PATH=qemu.log \
 PACKER_CACHE_DIR="${packer_cache_dir}" CHECKPOINT_DISABLE=1 \
 packer build -only=qemu ${@}
--- vbox.sh
+++ vbox.sh
@@ -6,5 +6,6 @@ if [ ! -d "${packer_cache_dir}" ]; then
     mkdir -p "${packer_cache_dir}"
 fi
 
+#PACKER_LOG=1 PACKER_LOG_PATH=vbox.log \
 PACKER_CACHE_DIR="${packer_cache_dir}" CHECKPOINT_DISABLE=1 \
 packer build -only=virtualbox ${@}
