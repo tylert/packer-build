3982cff9ec0222864e8a79eba68051c2d4a0e7b9
Chris <cgrandsjo@users.noreply.github.com>
2016-05-18 15:35:40 +0200

    Check if Ruby is installed

--- generate_templates.sh
+++ generate_templates.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+command -v ruby >/dev/null 2>&1 || { echo "ERROR: Please install Ruby." >&2; exit 1; }
+
 for template in $(find {debian,ubuntu} -name '*.yaml'); do
     if [ ! -f "${template}" ]; then continue; fi
     echo "Generating ${template/yaml/json}"
