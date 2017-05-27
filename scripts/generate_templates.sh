a17b212cf005993061ce8881eb3eb0bade937560
Tyler Tidman <tyler.tidman@draak.ca>
2017-05-27 18:50:32 -0400

    Prepare comments for adding additional network interfaces

--- scripts/generate_templates.sh
+++ scripts/generate_templates.sh
@@ -9,6 +9,7 @@ for template in $(find {debian,ubuntu} -name '*.yaml'); do
     if [ ! -f "${template}" ]; then continue; fi
     echo "Generating ${template/yaml/json}"
     ./scripts/yaml2json.rb < "${template}" > "${template/yaml/json}"
-    packer fix "${template/yaml/json}" > "${template/yaml/json}.new"
+    CHECKPOINT_DISABLE=1 packer fix "${template/yaml/json}" \
+        > "${template/yaml/json}.new"
     mv "${template/yaml/json}.new" "${template/yaml/json}"
 done
