89d30a66a5ae9f896a127d3e6c88be340631e677
Tyler Tidman <tyler.tidman@draak.ca>
2017-10-16 18:34:17 -0400

    Bump packer version to 1.1.1

--- scripts/fetch_hashicorp.sh
+++ scripts/fetch_hashicorp.sh
@@ -40,5 +40,5 @@ function fetch_hc_vagrant {
 }
 
 # Fetch binaries and install them locally
-fetch_hc_binary   'packer'   '1.1.0'  '/usr/local/bin'
+fetch_hc_binary   'packer'   '1.1.1'  '/usr/local/bin'
 fetch_hc_vagrant  'vagrant'  '2.0.0'
